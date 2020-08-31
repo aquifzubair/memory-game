ae853bffcf98025ec5a02a1e3c615726d4f8aeef
aquifzubair2 <aquif.zunair@mountblue.tech>
2020-09-01 01:36:47 +0530

    feat: impliments function to work it on gifs, adds local storage for counting the umber of click to get the score of player

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
 const gameContainer = document.getElementById("game");
+const headers = document.getElementById("header-id");
 
-const COLORS = [
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple",
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple",
+document.getElementsByClassName('btn')[0].addEventListener('click', ()=> window.location.reload())
+let myStorage = document.localStorage;
+
+const arrayOfGifAddress = [
+  'url(./gifs/1.gif)',
+  'url(./gifs/2.gif)',
+  'url(./gifs/3.gif)',
+  'url(./gifs/4.gif)',
+  'url(./gifs/5.gif)',
+  'url(./gifs/6.gif)',
+  'url(./gifs/7.gif)',
+  'url(./gifs/8.gif)',
+  'url(./gifs/9.gif)',
+  'url(./gifs/10.gif)',
+  'url(./gifs/11.gif)',
+  'url(./gifs/12.gif)',
+  'url(./gifs/1.gif)',
+  'url(./gifs/2.gif)',
+  'url(./gifs/3.gif)',
+  'url(./gifs/4.gif)',
+  'url(./gifs/5.gif)',
+  'url(./gifs/6.gif)',
+  'url(./gifs/7.gif)',
+  'url(./gifs/8.gif)',
+  'url(./gifs/9.gif)',
+  'url(./gifs/10.gif)',
+  'url(./gifs/11.gif)',
+  'url(./gifs/12.gif)'
 ];
 
 
 /**
- * Function to shuffle colors of array.<br>
+ * Function to shuarrayOfGifAddress of array.<br>
  * Based on an algorithm called Fisher Yates.
  * @param {array} array of colours
  * @return {array} array of shuffled array
  */
+
 
 function shuffle(array) {
   let counter = array.length;
@@ -41,7 +60,7 @@ function shuffle(array) {
   return array;
 }
 
-let shuffledColors = shuffle(COLORS);
+let shuarrayOfGifAddress = shuffle(arrayOfGifAddress);
 
 /**
  * Function loops over the array of colors<br>
@@ -51,13 +70,13 @@ let shuffledColors = shuffle(COLORS);
  * @returns {div} Returns number of div is equal to the length of color.
  */
 
-function createDivsForColors(colorArray) {
-  for (let color of colorArray) {
+function createDivsForGifs(gifArray) {
+  for (let gif of gifArray) {
     // create a new div
     const newDiv = document.createElement("div");
 
     // give it a class attribute for the value we are looping over
-    newDiv.classList.add(color);
+    newDiv.classList.add(gif);
 
     // call a function handleCardClick when a div is clicked on
     newDiv.addEventListener("click", handleCardClick);
@@ -73,6 +92,7 @@ let firstClick = true;
 let firstClickDiv;
 let count = 0;
 let handleDelay = false;
+let totalNumberOfClick = 0;
 
 /**
  * Method to handle events for every events.
@@ -83,20 +103,28 @@ let handleDelay = false;
 function handleCardClick(event) {
   // you can use event.target to see which element was clicked
   if (handleDelay) return;
+
   if (firstClick) {
     firstClickDiv = event.target;
-    firstClickDiv.style.backgroundColor = event.target.className;
-
+    firstClickDiv.style.backgroundImage = firstClickDiv.className;
+    firstClickDiv.style.backgroundSize = '100% 100%'
     firstClickDiv.style.pointerEvents = "none";
     firstClick = false;
+    totalNumberOfClick++;
   } else {
-    event.target.style.backgroundColor = event.target.className;
+    event.target.style.backgroundImage = event.target.className;
+    event.target.style.backgroundSize = '100% 100%'
     event.target.style.pointerEvents = "none";
     handleDelay = true;
+    totalNumberOfClick++;
     isCardMatch(firstClickDiv, event.target);
   }
 
-  if (count === 5) {
+  if (count === COLORS.length/2) {
+    const lastNumberOfTap = localStorage.getItem('numberOfTap')
+    if(totalNumberOfClick < +lastNumberOfTap){
+      localStorage.setItem('numberOfTap', `${totalNumberOfClick}`);
+    }
     restartGame();
   }
 }
@@ -108,8 +136,7 @@ function handleCardClick(event) {
 
 function restartGame() {
   setTimeout(() => {
-    if (confirm("You won the game, Do you wanna start the game")) {
-      console.log("logic of starting game");
+    if (confirm("You won the game, Do you wanna Restart the game")) {
       window.location.reload();
     }
   }, 1000);
@@ -130,8 +157,8 @@ function isCardMatch(firstCard, secondCard) {
     count++;
   } else {
     setTimeout(() => {
-      secondCard.style.backgroundColor = "";
-      firstCard.style.backgroundColor = "";
+      secondCard.style.backgroundImage = "";
+      firstCard.style.backgroundImage = "";
       firstCard.style.pointerEvents = "auto";
       secondCard.style.pointerEvents = "auto";
     }, 1000);
@@ -140,5 +167,18 @@ function isCardMatch(firstCard, secondCard) {
   setTimeout(() => (handleDelay = false), 1000);
 }
 
+/**
+ * Function to add an h3 which is containing score of the player.
+ * @returns An h3 containing best tap count of player.
+ */
+function addPlayerScore() {
+  let playerScore = document.createElement('h3');  
+  let textNode = document.createTextNode(`Your highest score is ${localStorage.getItem('numberOfTap')}`);
+  playerScore.appendChild(textNode);
+  headers.appendChild(playerScore);
+}
+
+addPlayerScore();
+
 // when the DOM loads
-createDivsForColors(shuffledColors);
+createDivsForGifs(shuarrayOfGifAddress);
